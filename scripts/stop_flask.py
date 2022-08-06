5e9c9bdfce121f68fe57bdf16d95927c8a288e47
taro.tsukamoto <taro.tsukamoto@cview.co.jp>
2022-08-07 00:15:28 +0900

    test;

--- scripts/stop_flask.py
+++ scripts/stop_flask.py
@@ -1,6 +1,8 @@
 import requests
 
 try:
+    print('try')
     requests.post("http://127.0.0.1/shutdown")
-except Exception:
-    pass
+except Exception as e:
+    print('in exception')
+    print(e)
